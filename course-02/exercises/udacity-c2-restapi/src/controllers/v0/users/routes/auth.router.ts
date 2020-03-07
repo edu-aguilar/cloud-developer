69680010c3cf44c64f4fd7bfb3f728afe5242956
Edu Aguilar <eduardo.aguilar@nc43tech.com>
2020-03-07 11:34:48 +0100

    feat(auth): add authentication using requireAuth middleware

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -7,6 +7,7 @@ import * as jwt from 'jsonwebtoken';
 import { NextFunction } from 'connect';
 
 import * as EmailValidator from 'email-validator';
+import { config } from '../../../../config/config';
 
 const router: Router = Router();
 
@@ -18,31 +19,29 @@ async function comparePasswords(plainTextPassword: string, hash: string): Promis
     return await bcrypt.compare(plainTextPassword, hash);
 }
 
-function generateJWT(user: User): string {
-    //@TODO Use jwt to create a new JWT Payload containing
-    return '';
+function generateJWT({dataValues}: User): string {
+    return jwt.sign(dataValues, config.dev.jwt_secret_key);
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
-    return next();
-    // if (!req.headers || !req.headers.authorization){
-    //     return res.status(401).send({ message: 'No authorization headers.' });
-    // }
-    
+    if (!req.headers || !req.headers.authorization){
+        return res.status(401).send({ message: 'No authorization headers.' });
+    }
 
-    // const token_bearer = req.headers.authorization.split(' ');
-    // if(token_bearer.length != 2){
-    //     return res.status(401).send({ message: 'Malformed token.' });
-    // }
-    
-    // const token = token_bearer[1];
+    const token_bearer = req.headers.authorization.split(' ');
 
-    // return jwt.verify(token, "hello", (err, decoded) => {
-    //   if (err) {
-    //     return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
-    //   }
-    //   return next();
-    // });
+    if(token_bearer.length != 2){
+        return res.status(401).send({ message: 'Malformed token.' });
+    }
+    
+    const token = token_bearer[1];
+
+    return jwt.verify(token, config.dev.jwt_secret_key, (err, decoded) => {
+      if (err) {
+        return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+      }
+      return next();
+    });
 }
 
 router.get('/verification', 
@@ -117,7 +116,7 @@ router.post('/', async (req: Request, res: Response) => {
     } catch (e) {
         throw e;
     }
-
+    
     // Generate JWT
     const jwt = generateJWT(savedUser);
 
